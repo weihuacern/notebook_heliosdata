2ad7efddefebd2332412a9e33b39b25022b248d7
Hua Wei <hua.wei@email.ucr.edu>
2018-07-25 17:52:28 -0700

    add it

--- script/cgi_query.py
+++ script/cgi_query.py
@@ -3,33 +3,46 @@ import time
 
 import requests
 
-#register, cgi-bin/register.py, ssn, name, phone
-#course, cgi-bin/course.py, ssn, name, course
-#eval, cgi-bin/eval.cgi, ssn, course, eval
-#info, cgi-bin/info.py, ssn
-MODE_APP_DICT = {
-    "register": ("cgi-bin/register.py", "?ssn=123&name=Jack Pitts&phone=1234567890"),
-    "course": ("cgi-bin/course.py", "?ssn=123&name=Jack Pitts&course=CS101"),
-    "eval": ("cgi-bin/eval.cgi", "?ssn=123&course=CS101&eval=A"),
-    "info": ("cgi-bin/info.py", "?ssn=123"),
+MODE_TAGS_DICT = {
+    "HR": ["set_employee", "set_payroll", "set_performance", "check_employee", "check_payroll", "check_performance"],
+    "SALES_MANAGER": ["set_sales", "set_customer", "check_sales", "check_customer", \
+                      "set_employee", "check_employee", "check_payroll"],
+    "SALES_GENERAL": ["set_customer", "check_sales", "check_customer", \
+                      "set_employee", "check_employee", "check_payroll"],
+}
+
+TAG_APP_DICT = {
+    "set_employee": ("cgi-bin/set_employee_info.py", "?eid=123&name=Jack Pitts&phone=1234567890"),
+    "set_payroll": ("cgi-bin/set_payroll.py", "?eid=123&payment=10000&title=SDE"),
+    "set_performance": ("cgi-bin/set_performance.py", "?eid=123&bonus=5000&attendance=50"),
+    "set_sales": ("cgi-bin/set_sales.py", "?eid=123&name=Jack Pitts&cids=1001"),
+    "set_customer": ("cgi-bin/set_customer.py", "?cid=123&contact=2345678901&trade=book"),
+    "check_employee": ("cgi-bin/check_employee_info.py", "?eid=123"),
+    "check_payroll": ("cgi-bin/check_payroll.py", "?eid=123"),
+    "check_performance": ("cgi-bin/check_performance.py", "?eid=123"),
+    "check_sales": ("cgi-bin/check_sales.py", "?eid=123"),
+    "check_customer": ("cgi-bin/check_customer.py", "?cid=123"),
 }
 
 if __name__ == "__main__":
-  mode = sys.argv[1]
-  print("running in mode: " + mode)
-  ip = "192.168.7.100"
-  try:
-      app = MODE_APP_DICT[mode][0]
-  except KeyError:
-      print("mode not found!")
-      sys.exit()
-  val = "123"
-  url = "http://" + ip + "/" + app + MODE_APP_DICT[mode][1]
-  #http://192.168.7.100/cgi-bin/info.py?ssn=123
-  print(url)
- 
-  while True:
-    time.sleep(2) 
-    r = requests.get(url)
-    #r = requests.get("https://github.com/")
-    print(r.content)
+    mode = sys.argv[1]
+    print("Script is running in mode: " + mode)
+    print("Valid run modes are: " + ",".join(MODE_TAGS_DICT.keys()))
+    ip = "192.168.7.100"
+    try:
+        tag_list = MODE_TAGS_DICT[mode]
+    except KeyError:
+        print("Not a valid mode!")
+        print("Valid run modes are: " + ",".join(MODE_TAGS_DICT.keys()))
+        sys.exit()
+
+    round_index = 0
+    while True:
+        round_index += 1
+        print("Round " + str(round_index) + ":" )
+        for tag in tag_list:
+            url = "http://" + ip + "/" + TAG_APP_DICT[tag][0] + TAG_APP_DICT[tag][1]
+            print(url)
+            r = requests.get(url)
+            print(r.content)
+            time.sleep(2)
